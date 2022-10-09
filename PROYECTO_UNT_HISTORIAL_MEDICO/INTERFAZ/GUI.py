283da534640989e0cf253f637d34d5060265a23e
hdjdudijxy <crisaaron_1@hotmail.com>
2022-10-09 13:02:20 -0500

    .

--- PROYECTO_UNT_HISTORIAL_MEDICO/INTERFAZ/GUI.py
+++ PROYECTO_UNT_HISTORIAL_MEDICO/INTERFAZ/GUI.py
@@ -1,7 +1,8 @@
+import tkinter
 from CONEXION.PacienteDao import DatosPaciente, editarDatoPaciente, guardarDatoPaciente, listar, listarCondicion, eliminarPaciente
 from CONEXION.HistorialDao import guardarHistoria, editarHistoria, eliminarHistoria, listarHistoria
 import tkinter as tk
-from tkinter import W, ttk, messagebox, Toplevel
+from tkinter import W, TclError, ttk, messagebox, Toplevel
 import tkcalendar as tc
 import datetime
 from PIL import Image, ImageTk
@@ -485,7 +486,7 @@ class Frame(tk.Frame):
         pdf.text(x=10, y=50, txt="DNI:")
         pdf.text(x=10, y=65, txt="Motivo de la visita a la clínica:")
         pdf.text(x=10, y=80, txt="Fecha y hora de la visita:")
-        pdf.text(x=10, y=95, txt="Su operación fue:")
+        pdf.text(x=10, y=95, txt="Su operación fue: ")
         pdf.text(x=10, y=110, txt="Debe seguir el siguiente tratamiento:")
         pdf.text(x=10, y=125, txt="Detalles adicionales:")
 
@@ -494,7 +495,7 @@ class Frame(tk.Frame):
         pdf.text(x=30, y=50, txt=g)
         pdf.text(x=100, y=65, txt=a)
         pdf.text(x=90, y=80, txt=f)
-        pdf.text(x=70, y=95, txt=b)
+        pdf.text(x=70, y=95, txt=f"{b} y debe de pagar  soles") # VOY A CREAR PRECIO EN LA BASE DE DATOS Y NO LO VOY A MOSTRAR EN LA TABLA SOLO PDF
         pdf.text(x=120, y=110, txt=c)
         pdf.text(x=80, y=125, txt=d)
 
@@ -514,12 +515,362 @@ class Frame(tk.Frame):
 
         pdf.output(f"HISTORIALES_PDF/Historial_{id}_{n}.pdf")
       
+    def TopLevelOperaciones(self):
+        """Método que genera el list box xon las operaciones"""
+
+        root=tk.Toplevel()
+
+        root.title("OPERACIONES MÉDICAS")
+        root.geometry("480x790+1030+15") 
+        root.resizable(0,0)
+        root.iconbitmap("ICONOS/icon.ico")
+        root.configure(background="bisque2") 
+
+        ################ CLASES FRAME ###############
+
+        ventana1=tk.Frame(root)
+        ventana1.configure(bg="bisque2",height=150)
+
+        ventana2=tk.LabelFrame(root)
+        ventana2.configure(text="Eliga la operación", font=("Verdana", 15, "bold"), border=5, bg="bisque4")
+
+        subventana1=tk.Frame(ventana2, bg="bisque4")
+        subventana2=tk.Frame(ventana2, bg="bisque4")
+
+        ########## CLASES ENTRYS ################
+
+        operacion=tk.StringVar()
+        
+        entry=tk.Entry(ventana1)
+        entry.configure(textvariable=operacion, font=("Verdana", 15), bg="bisque4", selectbackground="bisque3")
+
+        precio=tk.IntVar()
+
+        entry2=tk.Entry(ventana1)
+        entry2.configure(textvariable=precio, font=("Verdana", 15), bg="bisque4", selectbackground="bisque3")
+
+        ########## CLASES LABELS ############
+
+        etiqueta1=tk.Label(ventana1)
+        etiqueta1.configure(text="Operación a agregar", font=("Verdana", 15, "underline", "bold"),bg="bisque2")
+
+        etiqueta2=tk.Label(ventana1)
+        etiqueta2.configure(text="Monto a pagar", font=("Verdana", 15, "underline", "bold"),bg="bisque2")
+
+        ################ CLASE LISTBOX #####################
+
+        lista=tk.Listbox(subventana1)
+        lista.configure(bg="bisque3", selectbackground="navajowhite3", selectforeground="black", width=28, height=15,
+                        font=("Verdana", 15), cursor="hand2", justify=tk.LEFT, selectborderwidth=4)
+
+        diccionario_operaciones={"Reducción abierta de fractura con fijación interna":2000,"Laparotomía exploradora":3000, 
+        "Herniorrafia umbilical abierta":3215,"Reparación unilateral de hernia":325,"Apendicectomía":3025,"Incisión de tejido subcutáneo":525,
+        "Extirpación local":2325,"Sustitución de derivación ventricular":3225,"Reducción abierta de fractura":1325}
+
+        lista.insert(0,*diccionario_operaciones)
+
+        def agregar_datos():
+            """Función para agregar las operaciones y el precio tanto al diccionario como a el list box"""
+
+            Operacion=operacion.get()
+            Precio=precio.get()
+            
+            try:
+                if Operacion in diccionario_operaciones:
+                    
+                    titulo = "Agregar operación"
+                    mensaje = "Esta operación ya se encuentra en la base de datos"
+                    messagebox.showinfo(titulo, mensaje)
+                    self.topAHistoria.destroy()
+                    self.topHistoriaMedica.destroy()
+                    root.destroy()
+                    self.historiaMedica()
+                    self.topAgregarHistoria()   # No es necesario colocar el self.TopLevelOperaciones() ya que en este método ya referenciamos esa función
+                
+                elif Operacion==None:
+                    titulo = "Agregar operación"
+                    mensaje = "Inserte un valor para la operación"
+                    messagebox.showinfo(titulo, mensaje)
+                    self.topAHistoria.destroy()
+                    self.topHistoriaMedica.destroy()
+                    root.destroy()
+                    self.historiaMedica()
+                    self.topAgregarHistoria()
+                    
+                elif isinstance(Precio,int):
+
+                    diccionario_operaciones[Operacion]=Precio
+                    lista.insert(tk.END,Operacion)   
+
+                else:
+                    pass         
+
+            except:
+                titulo = "Agregar operación"
+                mensaje = "Error al agregar operación"
+                messagebox.showerror(titulo, mensaje)
+                self.topAHistoria.destroy()
+                self.topHistoriaMedica.destroy()
+                root.destroy()
+                self.historiaMedica()
+                self.topAgregarHistoria()
+                
+        def eliminar_datos():
+            """Función para eliminar datos del diccionario operaciones"""
+
+            tupla=(lista.curselection())
+
+            try:
+                del(diccionario_operaciones[lista.get(tupla[0])])
+                lista.delete(tupla[0])
+
+            except:
+                titulo = "Eliminar operación"
+                mensaje = "Error al eliminar la operación"
+                messagebox.showerror(titulo, mensaje)
+                self.topAHistoria.destroy()
+                self.topHistoriaMedica.destroy()
+                root.destroy()
+                self.historiaMedica()
+                self.topAgregarHistoria()
+                
+        def insertar_datos():
+            """Función para insertar las operaciones y el precio"""
+
+            tupla=(lista.curselection())
+            
+            value=lista.get(tupla[0])
+
+            try:
+                root.destroy()
+
+                self.svOperacion.set(value)
+                
+                # self.svPrecio.set(diccionario_operaciones[value])
+
+            except IndexError:
+                
+                titulo = "Insertar operación"
+                mensaje = "Error al insertar la operación"
+                messagebox.showerror(titulo, mensaje)
+
+        ########## CLASES BUTTON #############
+        
+        boton1=tk.Button(ventana1)
+        boton1.configure(text="AGREGAR", bg="orangered3", cursor="hand2", font=("Verdana", 12, "bold"), activebackground="orangered4", command=agregar_datos)
+
+        boton2=tk.Button(ventana1)
+        boton2.configure(text="ELIMINAR", bg="orangered3", cursor="hand2", font=("Verdana", 12, "bold"), activebackground="orangered4", command=eliminar_datos)
+
+        boton3=tk.Button(root)
+        boton3.configure(text="INSERTAR", bg="orangered3", cursor="hand2", font=("Verdana", 13, "bold"), activebackground="orangered4", width=28, command=insertar_datos)
+
+        scrollbar1 = tk.Scrollbar(subventana2) 
+        scrollbar1.configure(orient="vertical", command = lista.yview) 
+        lista.configure(yscrollcommand = scrollbar1.set) 
+        
+        scrollbar2 = tk.Scrollbar(subventana1)
+        scrollbar2.configure(orient="horizontal", command = lista.xview) 
+
+        lista.config(xscrollcommand = scrollbar2.set) 
+        
+        ventana1.pack(fill=tk.X)
+        ventana2.pack(padx=10,pady=10)
+        entry.place(x=40,y=36)
+        entry2.place(x=40,y=104)
+        boton1.place(x=325,y=34)
+        boton2.place(x=325,y=102)
+        etiqueta1.place(x=40,y=0)
+        etiqueta2.place(x=40,y=66)
+        boton3.pack(pady=6)
+        subventana1.pack(side = tk.LEFT, fill = tk.BOTH)
+        subventana2.pack(side = tk.RIGHT, fill = tk.BOTH)
+        lista.pack()
+        scrollbar1.pack(side = tk.RIGHT, fill = tk.BOTH)
+        scrollbar2.pack(side = tk.BOTTOM, fill = tk.BOTH)
+        
+    def TopLevelOperacionesEditar(self):
+        """Método que genera el list box xon las operaciones"""
+
+        root=tk.Toplevel()
+
+        root.title("OPERACIONES MÉDICAS")
+        root.geometry("480x790+1030+15") 
+        root.resizable(0,0)
+        root.iconbitmap("ICONOS/icon.ico")
+        root.configure(background="bisque2") 
+
+        ################ CLASES FRAME ###############
+
+        ventana1=tk.Frame(root)
+        ventana1.configure(bg="bisque2",height=150)
+
+        ventana2=tk.LabelFrame(root)
+        ventana2.configure(text="Eliga la operación", font=("Verdana", 15, "bold"), border=5, bg="bisque4")
+
+        subventana1=tk.Frame(ventana2, bg="bisque4")
+        subventana2=tk.Frame(ventana2, bg="bisque4")
+
+        ########## CLASES ENTRYS ################
+
+        operacion=tk.StringVar()
+        
+        entry=tk.Entry(ventana1)
+        entry.configure(textvariable=operacion, font=("Verdana", 15), bg="bisque4", selectbackground="bisque3")
+
+        precio=tk.IntVar()
+
+        entry2=tk.Entry(ventana1)
+        entry2.configure(textvariable=precio, font=("Verdana", 15), bg="bisque4", selectbackground="bisque3")
+
+        ########## CLASES LABELS ############
+
+        etiqueta1=tk.Label(ventana1)
+        etiqueta1.configure(text="Operación a agregar", font=("Verdana", 15, "underline", "bold"),bg="bisque2")
+
+        etiqueta2=tk.Label(ventana1)
+        etiqueta2.configure(text="Monto a pagar", font=("Verdana", 15, "underline", "bold"),bg="bisque2")
+
+        ################ CLASE LISTBOX #####################
+
+        lista=tk.Listbox(subventana1)
+        lista.configure(bg="bisque3", selectbackground="navajowhite3", selectforeground="black", width=28, height=15,
+                        font=("Verdana", 15), cursor="hand2", justify=tk.LEFT, selectborderwidth=4)
+
+        diccionario_operaciones={"Reducción abierta de fractura con fijación interna":2000,"Laparotomía exploradora":3000, 
+        "Herniorrafia umbilical abierta":3215,"Reparación unilateral de hernia":325,"Apendicectomía":3025,"Incisión de tejido subcutáneo":525,
+        "Extirpación local":2325,"Sustitución de derivación ventricular":3225,"Reducción abierta de fractura":1325}
+
+        lista.insert(0,*diccionario_operaciones)
+
+        def agregar_datos():
+            """Función para agregar las operaciones y el precio tanto al diccionario como a el list box"""
+
+            Operacion=operacion.get()
+            Precio=precio.get()
+            
+            try:
+                if Operacion in diccionario_operaciones:
+                    
+                    titulo = "Agregar operación"
+                    mensaje = "Esta operación ya se encuentra en la base de datos"
+                    messagebox.showinfo(titulo, mensaje)
+                    self.topEditarHistoria.destroy()
+                    self.topHistoriaMedica.destroy()
+                    root.destroy()
+                    self.historiaMedica()
+                    self.topEditarHistorialMedico()   # No es necesario colocar el self.TopLevelOperaciones() ya que en este método ya referenciamos esa función
+                
+                elif Operacion==None:
+                    titulo = "Agregar operación"
+                    mensaje = "Inserte un valor para la operación"
+                    messagebox.showinfo(titulo, mensaje)
+                    self.topEditarHistoria.destroy()
+                    self.topHistoriaMedica.destroy()
+                    root.destroy()
+                    self.historiaMedica()
+                    self.topEditarHistorialMedico()
+                    
+                elif isinstance(Precio,int):
+
+                    diccionario_operaciones[Operacion]=Precio
+                    lista.insert(tk.END,Operacion)   
+
+                else:
+                    pass         
+
+            except:
+                titulo = "Agregar operación"
+                mensaje = "Error al agregar operación"
+                messagebox.showerror(titulo, mensaje)
+                self.topEditarHistoria.destroy()
+                self.topHistoriaMedica.destroy()
+                root.destroy()
+                self.historiaMedica()
+                self.topEditarHistorialMedico()
+                
+        def eliminar_datos():
+            """Función para eliminar datos del diccionario operaciones"""
+
+            tupla=(lista.curselection())
+
+            try:
+                del(diccionario_operaciones[lista.get(tupla[0])])
+                lista.delete(tupla[0])
+
+            except:
+                titulo = "Eliminar operación"
+                mensaje = "Error al eliminar la operación"
+                messagebox.showerror(titulo, mensaje)
+                self.topEditarHistoria.destroy()
+                self.topHistoriaMedica.destroy()
+                root.destroy()
+                self.historiaMedica()
+                self.topEditarHistorialMedico()
+                
+        def insertar_datos():
+            """Función para insertar las operaciones y el precio"""
+
+            tupla=(lista.curselection())
+            
+            value=lista.get(tupla[0])
+
+            try:
+                root.destroy()
+
+                self.svOperacionEditar.set(value)
+                
+                # self.svPrecio.set(diccionario_operaciones[value])
+
+            except IndexError:
+                
+                titulo = "Insertar operación"
+                mensaje = "Error al insertar la operación"
+                messagebox.showerror(titulo, mensaje)
+
+        ########## CLASES BUTTON #############
+        
+        boton1=tk.Button(ventana1)
+        boton1.configure(text="AGREGAR", bg="orangered3", cursor="hand2", font=("Verdana", 12, "bold"), activebackground="orangered4", command=agregar_datos)
+
+        boton2=tk.Button(ventana1)
+        boton2.configure(text="ELIMINAR", bg="orangered3", cursor="hand2", font=("Verdana", 12, "bold"), activebackground="orangered4", command=eliminar_datos)
+
+        boton3=tk.Button(root)
+        boton3.configure(text="INSERTAR", bg="orangered3", cursor="hand2", font=("Verdana", 13, "bold"), activebackground="orangered4", width=28, command=insertar_datos)
+
+        scrollbar1 = tk.Scrollbar(subventana2) 
+        scrollbar1.configure(orient="vertical", command = lista.yview) 
+        lista.configure(yscrollcommand = scrollbar1.set) 
+        
+        scrollbar2 = tk.Scrollbar(subventana1)
+        scrollbar2.configure(orient="horizontal", command = lista.xview) 
+
+        lista.config(xscrollcommand = scrollbar2.set) 
+        
+        ventana1.pack(fill=tk.X)
+        ventana2.pack(padx=10,pady=10)
+        entry.place(x=40,y=36)
+        entry2.place(x=40,y=104)
+        boton1.place(x=325,y=34)
+        boton2.place(x=325,y=102)
+        etiqueta1.place(x=40,y=0)
+        etiqueta2.place(x=40,y=66)
+        boton3.pack(pady=6)
+        subventana1.pack(side = tk.LEFT, fill = tk.BOTH)
+        subventana2.pack(side = tk.RIGHT, fill = tk.BOTH)
+        lista.pack()
+        scrollbar1.pack(side = tk.RIGHT, fill = tk.BOTH)
+        scrollbar2.pack(side = tk.BOTTOM, fill = tk.BOTH)
+
     def topAgregarHistoria(self):
         """Función para generar el Top Level donde agregaremos los datos del historial"""
 
+        self.TopLevelOperaciones()
+
         self.topAHistoria = tk.Toplevel()
         self.topAHistoria.title("AGREGAR HISTORIA")
-        self.topAHistoria.geometry("900x450+300+350")
+        self.topAHistoria.geometry("900x450+100+370")
         self.topAHistoria.resizable(width=False, height=False)
         self.topAHistoria.iconbitmap("ICONOS/ICONO.ico")
         self.topAHistoria.config(background="cornsilk2")
@@ -611,6 +962,7 @@ class Frame(tk.Frame):
                 guardarHistoria(self.idPersonaHistoria, self.svFechaHistoria.get(),self.svMotivo.get(), self.svOperacion.get(), self.svTratamiento.get(),self.svDetalle.get())
             self.topAHistoria.destroy()
             self.topHistoriaMedica.destroy()
+            
             self.historiaMedica()
             self.idPersona = None
 
@@ -637,6 +989,9 @@ class Frame(tk.Frame):
     def topEditarHistorialMedico(self):
         
         try:
+
+            self.TopLevelOperacionesEditar()
+
             self.idHistoriaMedica = self.tabla2.item(self.tabla2.selection())["text"]
             self.fechaHistoriaEditar = str(datetime.date.today().strftime("%d/%m/%Y, ") + datetime.datetime.now().strftime("%I:%M %p"))
             self.motivoHistoriaEditar = self.tabla2.item(self.tabla2.selection())["values"][2]
@@ -645,7 +1000,7 @@ class Frame(tk.Frame):
             self.detalleHistoriaEditar = self.tabla2.item(self.tabla2.selection())["values"][5]
 
             self.topEditarHistoria = tk.Toplevel()
-            self.topEditarHistoria.geometry("910x450+300+350")
+            self.topEditarHistoria.geometry("910x450+100+370")
             self.topEditarHistoria.title("EDITAR HISTORIA MEDICA")
             self.topEditarHistoria.resizable(width=False, height=False)
             self.topEditarHistoria.iconbitmap("ICONOS/ICONO.ico")
