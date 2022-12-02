2b9de522f621cba9f1cdce80466e9a718984be4c
hdjdudixjxy <crisaaron_1@hotmail.com>
2022-12-01 23:37:25 -0500

    .

--- PROYECTO_UNT_HISTORIAL_MEDICO(MYSQL)/Conexion/conexion.py
+++ PROYECTO_UNT_HISTORIAL_MEDICO(MYSQL)/Conexion/conexion.py
@@ -3,13 +3,14 @@ import pymysql
 class ConexionDB:
     def __init__(self):
         self.conexion = pymysql.connect(
-            host='localhost',
-            user='root',
-            password='J30*l4c1V10',
-            db='classicmodels'
+            host='bj6rut9nh6zecwktf8pe-mysql.services.clever-cloud.com',
+            user='updoyj6wjsteofrk',
+            password='zIu4diili79hRYm7dsU5',
+            db='bj6rut9nh6zecwktf8pe'
             ) # nos conectamos a la base de datos
         self.cursor = self.conexion.cursor() # el metodo cursor nos permite modificar datos
 
     def cerrarConexion(self):
         self.conexion.commit() # subimos los datos insertados en los entrys
         self.conexion.close() # cerramos la conexión
+        
