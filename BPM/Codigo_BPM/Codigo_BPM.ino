0fa28b75f5fab6145957e9f64faa76fd6198a683
hdjdudixjxy <crisaaron_1@hotmail.com>
2023-01-22 18:27:13 -0500

    .

--- BPM/Codigo_BPM/Codigo_BPM.ino
+++ BPM/Codigo_BPM/Codigo_BPM.ino
@@ -16,7 +16,7 @@ int beatAvg;
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-
+     
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the display name (display)
 
 static const unsigned char PROGMEM logo2_bmp[] =
